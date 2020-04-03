3de3cb87828fe0cd7f0b073d9b80b596d29bb806
Abdulhakeem Mustapha <abdulhakeemmustapha@gmail.com>
2020-04-03 23:14:37 +0100

    Add install_required

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="couchdb", # Replace with your own username
-    version="0.0.2",
+    version="0.0.3",
     author="Abdulhakeem Mustapha",
     author_email="abdulhakeemmustapha@gmail.com",
     description="A package for handing basic CouchDB Operations",
@@ -17,6 +17,9 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'requests',
     ],
     python_requires='>=3.6',
 )
